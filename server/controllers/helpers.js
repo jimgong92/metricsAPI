6ab02632a859f228451186a123412b8be9c3a737
Jimmy Gong <gong.jim@gmail.com>
2015-05-02 00:05:53 -0700

    Moves pgsql seeder function into appropriate file, adds helper functions to convert JohnTime

---
+++ server/controllers/helpers.js
@@ -0,0 +1,23 @@
+var MS_PER_DAY = 86400000;
+var JT_VALUE = new Date(2009,00,01).valueOf();
+
+module.exports = {
+  /**
+   * @params: dateStr, String - ISO8601 date format 
+   * @return: johnTime, Number - JohnTime date format
+   */
+  convertToJT: function(dateStr){
+    var splitDate = dateStr.split('-');
+    var dateObj = new Date(splitDate[0], splitDate[1] - 1, splitDate[2]);
+    var johnTime = Math.floor((dateObj.valueOf() - JT_VALUE)/ MS_PER_DAY);
+    return johnTime;
+  },
+  /**
+   * @params: johnTime, Number - JohnTime date format
+   * @return: date, String - ISO8601 date format 
+   */
+  convertFromJT: function(johnTime){
+    var date = (johnTime * MS_PER_DAY + JT_VALUE).toISOString().slice(0, 10);
+    return date;
+  }
+}
