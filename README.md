d3d8b5177f19910127ae5d67062df9effb634049
Jimmy Gong <gong.jim@gmail.com>
2015-05-03 18:14:26 -0700

    Fills out router skeleton

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 RESTful API to query metrics dataset with dynamic query strings. Requests are cached to speed up future identical queries.
 
 ##Example Usage
+
 - (Instances) When did metric 167 have value greater than 500?
   e.g. 
   ```/api/167/instances/value>500```
+
 - (Count) How often did metric 15 have value equal to 1?
   e.g. 
   ```/api/15/count/value=1```
+
 - (Sum) How much value did metric 110 have on August 1st, 2014?
   e.g. 
   ```/api/110/sum/date=2014-08-01```
+  
 - (Average) On average, how much value did metric 110 have in March 2014?
   e.g. 
   ```/api/110/average/date>2015-03-01&date<2015-04-01```
--- server/config/routes.js
+++ server/config/routes.js
@@ -11,13 +11,28 @@ function router(app){
   app.get('/api/*', function(req, res){
     var queryString = req.params[0];
     cache.contains(queryString, function(doesExist){
+
+      //If route exists, send cached response
       if (doesExist){
         res.statusCode = 200;
         return cache.get(queryString, function(value){
           res.send(value);
         });
       }
+
+      //If route incomplete, send back 404
+      if (queryString.length < MIN_QUERY_LENGTH) return res.sendStatus(404);
+      var args = queryString.split('/');
+      if (args.length < 3) return res.sendStatus(404);
       
+      var filters = args[2].split('&');
+      var queryObj = {
+        metric_id: args[0],
+        type: args[1],
+        filters: filters,
+        toCache: queryString
+      };
+      metricsController.queryDB(queryObj, res);
     });
   });
 }
