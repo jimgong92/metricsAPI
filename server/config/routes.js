bf4bf31c81c280ee00ff2e7d56535f99dc7dab51
Jimmy Gong <gong.jim@gmail.com>
2015-05-03 18:10:13 -0700

    Adds cached response handling to router

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,25 @@
-function router(app){
+var metricsController = require('../controllers/metrics');
+var MIN_QUERY_LENGTH = '0/sum/value=0'.length;
+var cache = require('../cache');
 
+/**
+ * metric_id: Metric being observed
+ * type: 'instances' || 'count' || 'sum' || 'average'
+ * filter: Array of filters
+ */
+function router(app){
+  app.get('/api/*', function(req, res){
+    var queryString = req.params[0];
+    cache.contains(queryString, function(doesExist){
+      if (doesExist){
+        res.statusCode = 200;
+        return cache.get(queryString, function(value){
+          res.send(value);
+        });
+      }
+      
+    });
+  });
 }
 
 module.exports = router;
