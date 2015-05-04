65ae4ca336d5ce72ac422be89a2d665564add2ea
Jimmy Gong <gong.jim@gmail.com>
2015-05-03 18:37:08 -0700

    Adds queryString validation

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-#Metrics API
+# Metrics API
 RESTful API to query metrics dataset with dynamic query strings. Requests are cached to speed up future identical queries.
 
-##Example Usage
+## Table of Contents
+
+1. [Example Usage](#example-usage)
+2. [Development](#development)
+  1. [Installing Dependencies](#installing-dependencies)
+  2. [Updating Dataset](#updating-dataset)
+3. [API Components](#api-components)
+
+## Example Usage
 
 - (Instances) When did metric 167 have value greater than 500?
-  
   * e.g. ```/api/167/instances/value>500```
 
 - (Count) How often did metric 15 have value equal to 1?
-  
   * e.g. ```/api/15/count/value=1```
 
 - (Sum) How much value did metric 110 have on August 1st, 2014?
-  
   * e.g. ```/api/110/sum/date=2014-08-01```
 
 - (Average) On average, how much value did metric 110 have in March 2014?
-  
   * e.g. ```/api/110/average/date>2015-03-01&date<2015-04-01```
 
-##Development
+## Development
 
-###Installing Dependencies
+### Installing Dependencies
 - Install [Postgres.app](http://postgresapp.com/)
 - Run Postgres.app
 ```
@@ -29,10 +33,10 @@ npm install
 npm start
 ```
 
-###Changing/Updating Data
+### Updating Dataset
 To update the dataset for the API, replace the contents of metrics.csv with the updated content and restart the server. 
 
-##API Components
+## API Components
 
 - PostgreSQL
 - Express
--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,8 +1,49 @@
 var MS_PER_DAY = 86400000;
 var JT_VALUE = new Date(2009,00,01).valueOf();
 
+function getFilterObj(filterStr){
+  var condition = filterStr.match(/[<=>]/)[0];
+  var keyVal = filterStr.split(condition);
+  return {
+    key: keyVal[0],
+    condition: condition,
+    value: keyVal[1]
+  };
+}
+function isNum(val){
+  return !isNaN(Number(val));
+}
+function isValidType(type){
+  return type === 'instances' || type === 'count' || type === 'sum' || type === 'average';
+}
+function isValidFilter(filterStr){
+  var filterObj = getFilterObj(filterStr);
+  return isValidCondition(filterObj['condition'])
+    && isValidProp(filterObj['key'], filterObj['value']);
+}
+function isValidProp(key, val){
+  if (key === 'date'){
+    var arr = val.split('-');
+    if (val.length === 10 && arr.length === 3){
+      for (var i = 0; i < 3; i++){
+        if (!isNum(arr[i])) return false;
+      }
+      return true;
+    }
+    return false;
+  }
+  if (key === 'value') return isNum(val);
+  return false;
+}
+function isValidCondition(condition){
+  return condition === '<' || condition === '=' || condition === '>';
+}
 function isValid(query){
-
+  return isNum(query.metric_id) 
+    && isValidType(query.type) 
+    && query.filters.reduce(function(isTrue, filter){
+      return isValidFilter(filter) && isTrue;
+    }, true);
 }
 
 module.exports = {
@@ -25,5 +66,6 @@ module.exports = {
     var date = (johnTime * MS_PER_DAY + JT_VALUE).toISOString().slice(0, 10);
     return date;
   },
-  isValid: isValid
+  isValid: isValid,
+  getFilterObj: getFilterObj
 }
