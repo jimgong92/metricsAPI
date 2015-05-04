ada3e55ce72c684184ee85fc65534e16bf603b0c
Jimmy Gong <gong.jim@gmail.com>
2015-05-03 18:51:03 -0700

    Adds db querying

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,6 +1,40 @@
 var MS_PER_DAY = 86400000;
 var JT_VALUE = new Date(2009,00,01).valueOf();
 
+function getQuery(queryObj){
+  var where = {metric_id: queryObj.metric_id};
+  var valueFilters = queryObj.filters.join('').match(/value/g);
+  if (valueFilters) {
+    var isBetweenValue = valueFilters.length > 1;
+    var between = [];
+  }
+  queryObj.filters.forEach(function(filter){
+    var memo = helpers.getFilterObj(filter);
+    if (memo.key === 'date'){
+      memo.value = helpers.convertToJT(memo.value);
+      if (memo.condition === '='){
+        where['start_date'] = { gt: memo.value - 1 };
+        where['end_date'] = { lt: memo.value + 1 };
+      }
+      if (memo.condition === '>') where['start_date'] = { gt: memo.value };
+      if (memo.condition === '<') where['end_date'] = { lt: memo.value };
+    }
+    if (memo.key === 'value'){
+      if (memo.condition === '=') where['value'] = memo.value;
+      if (memo.condition === '>'){
+        if (isBetweenValue) between.push(memo.value + 1);
+        else where['value'] = { gt: memo.value };
+      }
+      if (memo.condition === '<'){
+        if (isBetweenValue) between.push(memo.value - 1);
+        else where['value'] = { lt: memo.value };
+      }
+    }
+  });
+  if (isBetweenValue) where['value'] = { between: between};
+  return {where: where};
+}
+
 function getFilterObj(filterStr){
   var condition = filterStr.match(/[<=>]/)[0];
   var keyVal = filterStr.split(condition);
@@ -67,5 +101,6 @@ module.exports = {
     return date;
   },
   isValid: isValid,
-  getFilterObj: getFilterObj
+  getFilterObj: getFilterObj,
+  getQuery: getQuery
 }
--- server/controllers/metrics.js
+++ server/controllers/metrics.js
@@ -4,8 +4,18 @@ var helpers = require('./helpers');
 var cache = require('../cache');
 
 function queryDB(queryObj, res){
-
+  if (!helpers.isValid(queryObj)) {
+    return res.send('Query missing necessary information');
+  }
+  /**
+   * Query database
+   */
+  Metric.all(helpers.getQuery(queryObj))
+    .then(function(results){
+      console.log(results);
+    });
 }
+
 
 module.exports = {
   queryDB: queryDB
